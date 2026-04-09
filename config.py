f783e0b29dcdbcdcdfb2834a47bbf40f1bde63e6
liquidbloomx <liquidbloom3301@gmail.com>
2026-04-10 01:11:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ USER_SESSION_STRING = ""
 CMD_SUFFIX = ""
 AUTHORIZED_CHATS = ""
 SUDO_USERS = ""
-DATABASE_URL = "mongodb+srv://perexdude:perexdude@cluster0.k0pvf3o.mongodb.net/?appName=Cluster0"
+DATABASE_URL = ""
 STATUS_LIMIT = 4
 DEFAULT_UPLOAD = "rc"
 STATUS_UPDATE_INTERVAL = 15
